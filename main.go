dc2372dd51021f67e8392501b558c15ee89b882a
hide0621 <hi19de72h2so4@gmail.com>
2022-06-17 11:47:14 +0900

    Todo.go　CreateTodo

--- main.go
+++ main.go
@@ -22,33 +22,38 @@ func main() {
 	//このコードの実行がトリガーとなり「users」と「todos」というテーブルがsqlite3上に作成される
 	fmt.Println(models.Db)
 
+	//「users」というtableのカラムに値を入れている（構造体Userのフィールドに紐付いている）
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+
+	//「users」というtableのカラムに値が入った状態（valuesの？の部分を埋める）にしてからコマンドの実行
+	u.CreateUser()
+
 	/*
-		//「users」というtableのカラムに値を入れている（構造体Userのフィールドに紐付いている）
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@example.com"
-		u.PassWord = "testtest"
+		//users tableにてidが「1」のユーザーを取得
+		u, _ := models.GetUser(1)
+
 		fmt.Println(u)
 
-		//「users」というtableのカラムに値が入った状態（valuesの？の部分を埋める）にしてからコマンドの実行
-		u.CreateUser()
+		//users tableのカラムの値を更新する
+		u.Name = "Test2"
+		u.Email = "test2@example.com"
+		u.UpdateUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
+
+		//users tableのカラムの値を消去する
+		u.DeleteUser()
+		//消去できているか確認
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
 	*/
 
-	//users tableにてidが「1」のユーザーを取得
-	u, _ := models.GetUser(1)
-
-	fmt.Println(u)
-
-	//users tableのカラムの値を更新する
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-
-	//users tableのカラムの値を消去する
-	u.DeleteUser()
-	//消去できているか確認
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	//前回「users」テーブルを作るセクションにてuserを作成の後、削除した
+	//userのIDは自動で増分されるので、今回のIDは2番とする
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
 }
